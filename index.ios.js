b30437caad5bf74ee5abd1bc0fe1768889fd2ee0
penn <pennsong07@gmail.com>
2016-03-30 21:01:22 +0800

    sub to friends after login

--- index.ios.js
+++ index.ios.js
@@ -1,52 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-
 import React, {
   AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
+  Component
 } from 'react-native';
 
-class ppwxClient extends Component {
+import PPRouter from './app/components/PPRouter';
+
+let APP = React.createClass({
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <PPRouter />
     );
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
 
-AppRegistry.registerComponent('ppwxClient', () => ppwxClient);
+AppRegistry.registerComponent('ppwxClient', () => APP);
